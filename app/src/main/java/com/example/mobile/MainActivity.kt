0b4a345d6f8f3f8bcfb68ef578b8acafdc6beddc
Hugo Cruz <dunkletodeus@gmail.com>
2025-10-16 20:18:15 +0200

    mayor parte de la pantalla inicial hecha animacion para las circulos emplementada posicion para los circulos mejora de los inputs mejora en coloreamento arreglo de la fuente posicionamento y mejora del logo

--- app/src/main/java/com/example/mobile/MainActivity.kt
+++ app/src/main/java/com/example/mobile/MainActivity.kt
@@ -1,21 +1,34 @@
 package com.example.mobile
 
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnimationUtils
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        iniciarAnimaciones()
+    }
+
+    private fun iniciarAnimaciones() {
+
+        val circle1 = findViewById<View>(R.id.circle1)
+        val circle2 = findViewById<View>(R.id.circle2)
+        val circle3 = findViewById<View>(R.id.circle3)
+        val circle4 = findViewById<View>(R.id.circle4)
+
+
+        val floatAnimation = AnimationUtils.loadAnimation(this, R.anim.float_animation)
+
+
+        circle1.startAnimation(floatAnimation)
+        circle2.startAnimation(floatAnimation)
+        circle3.startAnimation(floatAnimation)
+        circle4.startAnimation(floatAnimation)
     }
 }
-//test
