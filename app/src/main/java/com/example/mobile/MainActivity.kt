b0a7da092131589d92312d71eb302c40907955f9
Hugo Cruz <dunkletodeus@gmail.com>
2025-11-04 20:11:22 +0100

    Dimensiones rectangle y ProjectsActivity.kt

--- app/src/main/java/com/example/mobile/MainActivity.kt
+++ app/src/main/java/com/example/mobile/MainActivity.kt
@@ -22,7 +22,6 @@ class MainActivity : AppCompatActivity() {
         val circle1 = findViewById<View>(R.id.circle1)
         val circle2 = findViewById<View>(R.id.circle2)
         val circle3 = findViewById<View>(R.id.circle3)
-        val circle4 = findViewById<View>(R.id.circle4)
 
 
         val floatAnimation = AnimationUtils.loadAnimation(this, R.anim.float_animation)
@@ -31,6 +30,6 @@ class MainActivity : AppCompatActivity() {
         circle1.startAnimation(floatAnimation)
         circle2.startAnimation(floatAnimation)
         circle3.startAnimation(floatAnimation)
-        circle4.startAnimation(floatAnimation)
+
     }
 }
--- app/src/main/java/com/example/mobile/ProjectsActivity.kt
+++ app/src/main/java/com/example/mobile/ProjectsActivity.kt
@@ -1,6 +1,8 @@
 package com.example.mobile
 
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnimationUtils
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,22 @@ class ProjectsActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        iniciarAnimaciones()
+    }
+    private fun iniciarAnimaciones() {
+
+        val circle1 = findViewById<View>(R.id.circle1)
+        val circle2 = findViewById<View>(R.id.circle2)
+        val circle3 = findViewById<View>(R.id.circle3)
+        val circle4 = findViewById<View>(R.id.circle4)
+
+
+        val floatAnimation = AnimationUtils.loadAnimation(this, R.anim.float_animation)
+
+
+        circle1.startAnimation(floatAnimation)
+        circle2.startAnimation(floatAnimation)
+        circle3.startAnimation(floatAnimation)
+        circle4.startAnimation(floatAnimation)
     }
 }
