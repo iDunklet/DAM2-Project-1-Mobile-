543fb8cb6d4c1d2091ae33f890c0f25c419d2e94
Hugo Cruz <dunkletodeus@gmail.com>
2025-11-11 19:29:52 +0100

    unos cuantos cmabios y empezado pantalla de proyectos

--- app/src/main/java/com/example/mobile/ProjectsActivity.kt
+++ app/src/main/java/com/example/mobile/ProjectsActivity.kt
@@ -13,27 +13,25 @@ class ProjectsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_projects)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
         iniciarAnimaciones()
     }
-    private fun iniciarAnimaciones() {
 
+    private fun iniciarAnimaciones() {
         val circle1 = findViewById<View>(R.id.circle1)
         val circle2 = findViewById<View>(R.id.circle2)
         val circle3 = findViewById<View>(R.id.circle3)
-        val circle4 = findViewById<View>(R.id.circle4)
-
 
         val floatAnimation = AnimationUtils.loadAnimation(this, R.anim.float_animation)
-
 
         circle1.startAnimation(floatAnimation)
         circle2.startAnimation(floatAnimation)
         circle3.startAnimation(floatAnimation)
-        circle4.startAnimation(floatAnimation)
     }
 }
