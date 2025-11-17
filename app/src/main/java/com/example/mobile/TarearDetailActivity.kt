ca1bb97d7da2151869cd4fae515167233d93c244
jmarti <cxiang2425@politecnics.barcelona>
2025-11-17 20:18:05 +0100

    upsate activity TarearDetailActivity y ProjectDetailActivity

--- app/src/main/java/com/example/mobile/TarearDetailActivity.kt
+++ app/src/main/java/com/example/mobile/TarearDetailActivity.kt
@@ -1,6 +1,7 @@
 package com.example.mobile
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -9,15 +10,25 @@ import java.util.*
 
 class TarearDetailActivity : AppCompatActivity() {
 
+    private var startTime: Long = 0L
+    private var isTiming = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tarear_detail)
 
         val txtName = findViewById<TextView>(R.id.txtTaskName)
         val txtDescription = findViewById<TextView>(R.id.txtTaskDescription)
+        val txtTime = findViewById<TextView>(R.id.txtTiempo)
         val txtUser = findViewById<TextView>(R.id.txtTaskUser)
         val txtDates = findViewById<TextView>(R.id.txtTaskDates)
         val btnBack = findViewById<ImageView>(R.id.btnBack)
+        val btnEmpezar = findViewById<Button>(R.id.btnEmpezar)
+        val btnTerminar = findViewById<Button>(R.id.btnTerminal)
+        val txtSinEmpezar = findViewById<TextView>(R.id.txtSinEmpezar)
+
+
+
 
 
         btnBack.setOnClickListener { onBackPressedDispatcher.onBackPressed() }
@@ -36,5 +47,27 @@ class TarearDetailActivity : AppCompatActivity() {
         txtDescription.text = taskDescription
         txtUser.text = "Asignado a: $taskUser"
         txtDates.text = "Desde $startDate hasta $endDate"
+
+        btnEmpezar.setOnClickListener {
+            startTime = System.currentTimeMillis()
+            isTiming = true
+            txtTime.text = "Empezado"
+            txtSinEmpezar.setTextColor(resources.getColor(R.color.Turquesa))
+
+        }
+
+        btnTerminar.setOnClickListener {
+            val endTime = System.currentTimeMillis()
+            val elapsed = endTime - startTime
+            val totalSeconds = (elapsed / 1000).toInt()
+            val minutes = totalSeconds / 60
+            val seconds = totalSeconds % 60
+
+            val timeString = String.format("%d:%02d", minutes, seconds)
+            txtTime.text = "Terminado: $timeString"
+        }
+
+
+        }
     }
-}
+
